c5e11638f89e84366c0bc4edd39574bb3aea5121
ackoshy <59678079+ackoshy@users.noreply.github.com>
2022-02-10 01:07:54 -0800

    annotated bib

--- 157bportal/friendsList/script.js
+++ 157bportal/friendsList/script.js
@@ -2,72 +2,37 @@ Parse.initialize("UP0VGtqzEiybYxUdnlZJRmdcbIFa3JbIhblmXZN2","BPOH7LmPpiLj6AHhM5h
 Parse.serverURL = 'https://parseapi.back4app.com/'
 const newBtn = document.getElementById("newbtn"); 
 const editBtns = document.querySelectorAll(".fa-edit"); 
-const addFriendForm = document.getElementById("add-friend"); 
+const addFriendForm = document.getElementById("entry-form"); 
 const editFriendForm = document.getElementById("edit-friend"); 
 const friendList = document.querySelector("main ol"); 
-try{
+const inputs = document.querySelectorAll("#entry-form input:not([type=submit])"); 
 
-
-async function displayFriends(){
+async function displayEntry(){
     const friends = Parse.Object.extend("Friends"); 
     const query = new Parse.Query(friends); 
-    const results = await query.ascending("lname").find(); 
+    const results = await query.ascending("entry").find(); 
     console.log(results); 
 
-    results.forEach(function(eachFriend){
-        const id = eachFriend.id; 
-        const lname = eachFriend.get('lname'); 
-        const fname = eachFriend.get('fname'); 
-        const email = eachFriend.get('email'); 
-        const facebook = eachFriend.get('facebook'); 
-        const twitter = eachFriend.get('twitter'); 
-        const instagram = eachFriend.get('instagram'); 
-        const linkedin = eachFriend.get('linkedin'); 
-
+    results.forEach(function(eachEntry){
+        const id = eachEntry.id; 
+        const entry = eachEntry.get('lname'); 
+        const date = eachEntry.get('fname'); 
         const theListItem = document.createElement('li'); 
         theListItem.setAttribute("id", `r-${id}`); 
         theListItem.innerHTML = `
         <li>
-        <div class="name"> ${fname} ${lname}
+        <div class="name"> ${date} <div><button onclick="myFunction()" id="myBtn">View Entry</button></div> <span id="dots"></span><span id="more">${entry}</span>
         </div>
-        <div class="email">
-            <i class="fas fa-envelope-square"></i> ${email}
-        </div>
-        <div class="social">
-            <a href="${facebook}"><i class="fab fa-facebook-square"></i></a>
-            <a href="${twitter}"><i class="fab fa-twitter-square"></i></a>
-            <a href="${instagram}"><i class="fab fa-instagram"></i></a>
-            <a href="${linkedin}"><i class="fab fa-linkedin"></i></a>
-        </div>
-        <i class="fas fa-edit" id="e-${id}"></i>
-        <i class="fas fa-times-circle id="d-${id}"></i>
     </li>`
-
     friendList.append(theListItem); 
-
-    }); 
-
-     
-
+    });      
 }
-displayFriends(); 
-
-}   catch(error){
-        console.log("error while fetching friends", error); 
-    }
-
-
-
-newBtn.addEventListener('click', function(event){
-    event.preventDefault(); 
-    addFriendForm.className = "add-friend-onscreen"; 
-}); 
+displayEntry(); 
 
 addFriendForm.addEventListener('submit', function(event){
     event.preventDefault(); 
-    addFriendForm.className = "add-friend-offscreen"; 
-
-
+    console.log;("submitting new friend"); 
+    addFriend(); 
 
 }); 
 
@@ -78,7 +43,53 @@ for(let i=0; i<editBtns.length; i++){
     }); 
 }
 
-editFriendForm.addEventListener('submit', function(event){
-    event.preventDefault(); 
-    editFriendForm.className = "edit-friend-offscreen"; 
-}); 
+
+async function addFriend(){
+    console.log("in add friend"); 
+    const newEntry = {}; 
+
+    for(let i=0; i<inputs.length; i++){
+        let key = inputs[i].getAttribute("name"); 
+        let value = inputs[i].value; 
+        newEntry[key] = value; 
+    }
+
+
+    if(newEntry.date != "" && newEntry.entry != "" ){
+       const newEntryData = new Parse.Object('Friends'); 
+       newEntryData.set('date', newEntry.date); 
+       newEntryData.set('entry', newEntry.entry); 
+
+       const result = await newEntryData.save(); 
+       resetFormFields(); 
+       /* addFriendForm.className = "add-friend-offscreen";  */
+       friendList.innerHTML = ''; 
+       displayEntry(); 
+       
+    }
+    else{
+        /* addFriendForm.className = "add-friend-offscreen";  */
+    }
+
+    function resetFormFields(){
+        document.getElementById("date").value = ""; 
+        document.getElementById("entry").value = ""; 
+    }
+}
+
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "View Entry";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
+
