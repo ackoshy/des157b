da2eb6e97323ae33369334ba6bfd102cde59d285
ackoshy <59678079+ackoshy@users.noreply.github.com>
2022-02-14 14:46:25 -0800

    fixing link

--- 157bportal/st3/script.js
+++ 157bportal/st3/script.js
@@ -1,19 +1,1 @@
-$(".flipbookOne").turn({
-         
-    autoCenter: true
-});
-
-
-$(".flipbookTwo").turn({
-   
-    autoCenter: true
-});
-$(".flipbookThree").turn({
-   
-    autoCenter: true
-});
-$(".flipbookFour").turn({
-   
-    autoCenter: true
-}); 
 
