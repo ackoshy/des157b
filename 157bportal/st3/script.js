5d3c2de82ec017c2eef0271a3c6a7bbfc72af2a1
ackoshy <59678079+ackoshy@users.noreply.github.com>
2022-02-14 14:28:15 -0800

    pushing st3

---
+++ 157bportal/st3/script.js
@@ -0,0 +1,19 @@
+$(".flipbookOne").turn({
+         
+    autoCenter: true
+});
+
+
+$(".flipbookTwo").turn({
+   
+    autoCenter: true
+});
+$(".flipbookThree").turn({
+   
+    autoCenter: true
+});
+$(".flipbookFour").turn({
+   
+    autoCenter: true
+}); 
+
